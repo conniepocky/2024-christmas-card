8b3dc8d98113ad21af02a0a98ff8421d1d20964d
Connie <c.amer@icloud.com>
2024-11-28 18:23:12 +0000

    main snowglobe

--- index.js
+++ index.js
@@ -13,8 +13,42 @@ button.addEventListener('click', (event) => {
     document.body.style.backgroundImage = 'none';
     document.body.style.backgroundColor = 'black';
 
-    title.innerHTML = `Hello ${name}!`;
+    title.innerHTML = `Merry Christmas ${name}!`;
 
     content.classList.add('hidden');
     globeContainer.classList.remove('hidden');
 });
+
+let globe = document.getElementById("snowglobe");
+let particles;
+
+tsParticles.loadJSON('particles', 'particles.json')
+    .then(function () {
+    particles = tsParticles.domItem(0);
+    particles.pause()
+
+    globe.addEventListener('click', () => {
+        shakeItUp()
+    });
+
+    window.addEventListener("devicemotion", (event) => {
+        const acceleration = calculateAcceleration(event);
+
+        if (acceleration > 23) {
+        shakeItUp()
+        }
+    });
+});
+
+function calculateAcceleration(event) {
+    const {x, y, z} = event.accelerationIncludingGravity;
+    const accelerationMagnitude = Math.sqrt(x * x + y * y + z * z);
+    return accelerationMagnitude;
+}
+
+function shakeItUp() {
+    particles.play();
+    
+    globe.classList.add("shake")
+    setTimeout(() => {globe.classList.remove("shake")}, 1100)
+}
